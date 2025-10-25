562e2d7793c031346c6b1cc77f104014a0363b82
YeonhaPark <yonaprisca@gmail.com>
2025-10-25 13:03:48 +0900

    fix: remove point events none

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <div className="absolute z-0">
           <GameClient />
         </div>
-        <div className="relative  z-10 mt-[700px] flex flex-col items-center pointer-events-none">
+        <div className="relative  z-10 mt-[700px] flex flex-col items-center">
           <About />
         </div>
       </div>
